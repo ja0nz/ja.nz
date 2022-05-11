de2e7792f6630d7af176e0732b52cb80db5d1619
ja0nz <mail@ja.nz>
2022-05-12 01:21:01 +0200

    api: implemented second layer cache

--- api/src/handlers/handlers.ts
+++ api/src/handlers/handlers.ts
@@ -1,13 +1,21 @@
 import { push, transduce } from '@thi.ng/transducers'
 import { Issue } from 'gh-cms-ql'
 import { fetchAll, fetchQl, queryIPager, queryISing } from '../graphql'
+const cKey = 'http://int.dummy'
 
 export const handlerPager = (xform: any, headers: any) =>
   async function ({ params }: Query, env: Env): Promise<Response> {
     const id = params?.id
     let body: Array<Issue> | Issue = []
+    const cache = caches.default
+    const inCache = await cache.match(cKey)
     try {
-      body = await fetchQl(env, fetchAll(queryIPager))
+      if (inCache) {
+        body = await inCache.json()
+      } else {
+        body = await fetchQl(env, fetchAll(queryIPager))
+        await cache.put(cKey, new Response(JSON.stringify(body), { headers }))
+      }
     } catch (e) {
       console.error(`ERROR: GitHub ${JSON.stringify(e)}`)
       return new Response('Could not fetch GitHub', { status: 500 })
--- front/src/routes/[slug].ts
+++ front/src/routes/[slug].ts
@@ -4,8 +4,8 @@ import { transduce, last } from "@thi.ng/transducers";
 export async function get({ params }) {
   const slug = params.slug;
   if (!Number(slug)) return { status: 404 };
-  const url = `https://api.ja.nz/v1`;
-  const response = await fetch(url);
+  const url = (id: string) => `https://api.ja.nz/v1/parsed/title/${id}`;
+  const response = await fetch(url(slug));
   const json = response.ok ? await response.json() : [];
   const content = transduce(getMDContent(params.slug), last(), json);
   return {
