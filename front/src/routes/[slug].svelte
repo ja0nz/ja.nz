1f9102a2447d89f69c56659bd73621e4ae7e22b1
ja0nz <mail@ja.nz>
2022-05-12 14:16:34 +0200

    feat: slug; error cases

--- front/src/routes/[slug].svelte
+++ front/src/routes/[slug].svelte
@@ -2,15 +2,17 @@
   import type { LoadInput } from "@sveltejs/kit";
   import type { ParsedIssue, SlugOutput } from "src/app";
   export const prerender = true;
+  const url = (id: string) => `https://api.ja.nz/v1/parsed/title/${id}`;
   export async function load({
     params,
     fetch,
   }: LoadInput): Promise<SlugOutput> {
     const slug = params.slug;
     if (!Number(slug)) return { status: 404 };
-    const url = (id: string) => `https://api.ja.nz/v1/parsed/title/${id}`;
     const response = await fetch(url(slug));
-    const [content]: [ParsedIssue] = response.ok && (await response.json());
+    if (!response.ok) return { status: 500 };
+    const [content]: [ParsedIssue] = await response.json();
+    if (!content) return { status: 404 }
 
     return {
       status: response.status,
