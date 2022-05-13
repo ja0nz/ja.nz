7209e6b63de388020182f74208bfad2c6a7527f5
ja0nz <mail@ja.nz>
2022-05-13 12:38:00 +0200

    feat: Remove not needed stuff

--- front/src/routes/[slug=integer].svelte
+++ front/src/routes/[slug=integer].svelte
@@ -1,15 +1,12 @@
 <script context="module" lang="ts">
   import type { LoadInput } from "@sveltejs/kit";
   import type { ParsedIssue, SlugOutput } from "src/app";
-  export const prerender = true;
-  const url = (id: string) => `https://api.ja.nz/v1/parsed/title/${id}`;
   export async function load({
     params,
     fetch,
   }: LoadInput): Promise<SlugOutput> {
-    const slug = params.slug;
-    if (!Number(slug)) return { status: 404 };
-    const response = await fetch(url(slug));
+    const url = `https://api.ja.nz/v1/parsed/title/${params.slug}`;
+    const response = await fetch(url);
     if (!response.ok) return { status: 500 };
     const [content]: [ParsedIssue] = await response.json();
     if (!content) return { status: 404 }
--- front/svelte.config.js
+++ front/svelte.config.js
@@ -1,6 +1,4 @@
-//import adapter from "@sveltejs/adapter-cloudflare";
 import adapter from "@sveltejs/adapter-cloudflare-workers";
-//import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,27 +9,8 @@ const config = {
 
   kit: {
     adapter: adapter(),
-    //vite: {
-    // resolve: {
-    //   alias: {
-    //     TODO: package.json
-    //     path: "path-browserify",
-    //   },
-    // },
-    // optimizeDeps: {
-    //   esbuildOptions: {
-    //     plugins: [
-    //       NodeGlobalsPolyfillPlugin({
-    //         buffer: true,
-    //       }),
-    //     ],
-    //   },
-    // },
-    //},
-
-    // Override http methods in the Todo forms
-    methodOverride: {
-      allowed: ["PATCH", "DELETE"],
+    prerender: {
+      default: true,
     },
   },
 };
