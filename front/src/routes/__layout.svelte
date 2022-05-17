b4350b152e7ad0d017f90295145069defd2e11d3
ja0nz <mail@ja.nz>
2022-05-18 00:35:22 +0200

    front: CSS Wireframe

--- front/src/routes/__layout.svelte
+++ front/src/routes/__layout.svelte
@@ -1,16 +1,10 @@
 <script>
   import "../css/cube.css";
-  import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
+  //import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
 </script>
 
 <svelte:head />
 
 <main class="wrapper">
-  <ThemeSwitch />
   <slot />
-  <footer>
-    <p>
-      visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-    </p>
-  </footer>
 </main>
--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -16,6 +16,7 @@ module.exports = {
       light: "#f3f3f3",
       dark: "#252525",
     },
+    // Theme
     spacing: {
       "size-0": "clamp(0.83rem, 0.90rem + -0.32vw, 0.67rem)",
       base: "clamp(1.00rem, 1.00rem + 0.00vw, 1.00rem)",
@@ -23,7 +24,7 @@ module.exports = {
       "size-2": "clamp(1.44rem, 1.12rem + 1.58vw, 2.25rem)",
       "size-3": "clamp(1.73rem, 1.09rem + 3.21vw, 3.38rem)",
       "size-4": "clamp(2.07rem, 0.91rem + 5.83vw, 5.06rem)",
-      "size-5": "clamp(7.59rem, 0.50rem + 9.96vw, 2.49rem)",
+      "size-5": "clamp(2.49rem, 0.50rem + 9.96vw, 7.59rem)",
     },
     fontSize: {
       "size-0": "clamp(0.83rem, 0.90rem + -0.32vw, 0.67rem)",
@@ -32,8 +33,27 @@ module.exports = {
       "size-2": "clamp(1.44rem, 1.12rem + 1.58vw, 2.25rem)",
       "size-3": "clamp(1.73rem, 1.09rem + 3.21vw, 3.38rem)",
       "size-4": "clamp(2.07rem, 0.91rem + 5.83vw, 5.06rem)",
-      "size-5": "clamp(7.59rem, 0.50rem + 9.96vw, 2.49rem)",
+      "size-5": "clamp(2.49rem, 0.50rem + 9.96vw, 7.59rem)",
     },
+    // Own
+    // spacing: {
+    //   "size-0": "clamp(0.83rem, 0.90rem + -0.32vw, 0.67rem)",
+    //   base: "clamp(1.00rem, 1.00rem + 0.00vw, 1.00rem)",
+    //   "size-1": "clamp(1.20rem, 1.08rem + 0.59vw, 1.50rem)",
+    //   "size-2": "clamp(1.44rem, 1.12rem + 1.58vw, 2.25rem)",
+    //   "size-3": "clamp(1.73rem, 1.09rem + 3.21vw, 3.38rem)",
+    //   "size-4": "clamp(2.07rem, 0.91rem + 5.83vw, 5.06rem)",
+    //   "size-5": "clamp(7.59rem, 0.50rem + 9.96vw, 2.49rem)",
+    // },
+    // fontSize: {
+    //   "size-0": "clamp(0.83rem, 0.90rem + -0.32vw, 0.67rem)",
+    //   base: "clamp(1.00rem, 1.00rem + 0.00vw, 1.00rem)",
+    //   "size-1": "clamp(1.20rem, 1.08rem + 0.59vw, 1.50rem)",
+    //   "size-2": "clamp(1.44rem, 1.12rem + 1.58vw, 2.25rem)",
+    //   "size-3": "clamp(1.73rem, 1.09rem + 3.21vw, 3.38rem)",
+    //   "size-4": "clamp(2.07rem, 0.91rem + 5.83vw, 5.06rem)",
+    //   "size-5": "clamp(7.59rem, 0.50rem + 9.96vw, 2.49rem)",
+    // },
     fontFamily: {
       sans: [
         '"Inter"',
