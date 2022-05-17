2cf461da7a5f563e7cbbd40f23ff9b07fe5dee0e
ja0nz <mail@ja.nz>
2022-05-17 10:34:22 +0200

    WIP

--- front/postcss.config.cjs
+++ front/postcss.config.cjs
@@ -1,4 +1,5 @@
 const tailwind = require("tailwindcss");
+const nesting = require("tailwindcss/nesting");
 const autoprefixer = require("autoprefixer");
 const purgecss = require("@fullhuman/postcss-purgecss");
 const tw = require("./tailwind.config.cjs");
@@ -7,6 +8,7 @@ const PROD = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
+    nesting(),
     // Some plugins, like postcss-nested, need to run before Tailwind
     tailwind(),
     // But others, like autoprefixer, need to run after
