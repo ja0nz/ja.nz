df2005730c8600187cbf8257a083c727c8170b4c
ja0nz <mail@ja.nz>
2022-05-18 14:28:55 +0200

    feat(front): mobile CSS behaviour

--- front/src/routes/index.svelte
+++ front/src/routes/index.svelte
@@ -1,27 +1,34 @@
 <script>
+  import { browser, dev } from "$app/env";
+  // start scrolled down
+  browser && window.scrollTo(0, document.body.scrollHeight);
   let tags = ["typescript", "purescript", "nixOS"];
+  //let tags = ["typescript", "purescript", "nixOS", "typescript", "purescript", "nixOS", "typescript", "purescript", "nixOS", "typescript", "purescript", "nixOS"];
 </script>
 
-<article class="sidebar">
+<article class="sidebar | mobile-sidebar">
   <div class="menu | sticky-header">
-    <div class="box">image, bla</div>
-    <div class="tagSearch">Fuzzy tag search</div>
+    <div class="box">Profileimage & last seen</div>
+    <div>Fuzzy tag search</div>
     <div class="overflow-y-auto">
       {#each tags as tag}
         <div class="box">{tag}</div>
       {/each}
     </div>
   </div>
-  <div id="not-sidebar" class="">
+  <div id="not-sidebar" class="no-contentX">
     <div class="key-header | splitter sticky-header">
-      <div class="box">Topic Card Photo blah</div>
+      <div class="box">⬅ Topic Card Photo blah</div>
       <aside>
-        <div class="search box">Search</div>
-        <div class="themeswitch">ThemeSwitch</div>
+        <div>Search</div>
+        <div>ThemeSwitch</div>
       </aside>
     </div>
-    <div style="min-height: 1500px;" class="overflow-y-auto box">
+    <div style="min-height: 1000px;" class="box">
       Content Area
+    </div>
+    <div class="box">
+      Send me a message
     </div>
   </div>
 </article>
