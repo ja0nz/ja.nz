64b9d62c031e008f30bc4f346bad8c5a470fe1ef
ja0nz <mail@ja.nz>
2022-05-30 20:06:08 +0200

    Bumped deps

--- shell.nix
+++ shell.nix
@@ -1,16 +1,22 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (self: super: {
+      yarn = super.yarn.override { nodejs = pkgs.nodejs-18_x; };
+    })
+  ];
+} }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-   nodejs-18_x
-   nodePackages.yarn
-   nodePackages.typescript-language-server
-   nodePackages.vscode-html-languageserver-bin
-   nodePackages.vscode-css-languageserver-bin
-   nodePackages.vscode-json-languageserver
-   nodePackages.svelte-language-server
-   nodePackages.svelte-check
-   nodePackages.prettier
-   nodePackages.eslint
+    yarn
+    nixfmt
+    nodePackages.typescript-language-server
+    nodePackages.vscode-html-languageserver-bin
+    nodePackages.vscode-css-languageserver-bin
+    nodePackages.vscode-json-languageserver
+    nodePackages.svelte-language-server
+    nodePackages.svelte-check
+    nodePackages.prettier
+    nodePackages.eslint
   ];
 }
