214ce7a2d52bacffe291d0a5537e9d2a8e1670bc
ja0nz <mail@ja.nz>
2023-01-16 15:17:27 +0100

    fix: restricted fuzzy filter

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -2,10 +2,17 @@ import type { Fn } from "@thi.ng/api";
 import { filterFuzzy } from "@thi.ng/transducers";
 
 /* Create Filter */
-export const filterF =
-  <T>(target: T[], getFn: Fn<T, string>) =>
-  (search: string) =>
-    [...filterFuzzy(search, { key: getFn }, target)];
+export const filterF = <T>(target: T[], getFn: Fn<T, string>) => {
+  const fn = (s: T) => getFn(s).toLowerCase();
+  const all = target.reduce(
+    (acc: string[], x) => acc.concat(fn(x).split(" ")),
+    []
+  );
+  return (search: string) => {
+    const uniq = [...filterFuzzy(search.toLowerCase(), all)];
+    return target.filter((x) => uniq.some((y) => fn(x).includes(y)));
+  };
+};
 
 export const filter =
   <T>(target: T[], getFn: Fn<T, string>) =>
