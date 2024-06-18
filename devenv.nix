3aba2fd2d48cd9d559e7e2dd743ceeb0257486cb
ja0nz <git@ja.nz>
2024-06-18 11:59:37 +0200

    chore: updated devenv

--- devenv.nix
+++ devenv.nix
@@ -11,15 +11,17 @@
     nodePackages.ts-node
   ];
 
-   # https://devenv.sh/scripts/
+  # https://devenv.sh/scripts/
   scripts = with pkgs; {
-    run-fmt.exec = "${nodePackages.prettier}/bin/prettier . --write --plugin-search-dir=$DEVENV_ROOT";
+    run-fmt.exec =
+      "${nodePackages.prettier}/bin/prettier . --write --plugin-search-dir=$DEVENV_ROOT";
     run-repl.exec = "${nodePackages.ts-node}/bin/ts-node";
     run-lint.exec = "${nodePackages.eslint}/bin/eslint .";
     run-check.exec = "cd $DEVENV_ROOT && ${yarn}/bin/yarn astro check";
     run-build.exec = "cd $DEVENV_ROOT && ${yarn}/bin/yarn astro build";
     run-preview.exec = "cd $DEVENV_ROOT && ${yarn}/bin/yarn astro preview";
-    run-dev.exec = "cd $DEVENV_ROOT && NODE_OPTIONS='--inspect' ${yarn}/bin/yarn astro dev";
+    run-dev.exec =
+      "cd $DEVENV_ROOT && NODE_OPTIONS='--inspect' ${yarn}/bin/yarn astro dev";
     run-clean.exec = "rm -rf dist";
   };
 
